e39db70f09f9899442e435087536b2b656b17a3b
code-env <org.codeen@gmail.com>
2023-08-14 15:41:32 +0100

    finished the hero section

--- src/component/Hero.tsx
+++ src/component/Hero.tsx
@@ -1,16 +1,29 @@
 import React from "react";
 import "@/styles/hero.scss";
+import Image from "next/image";
+import dashboard from "../../public/dashboard.svg";
 
 const Hero = () => {
   return (
     <div className="hero">
       <section className="hero__left">
         <h1 className="left__text">
-          LEARN SHARE <span>CONNECT</span>
+          LEARN <br /> SHARE <br /> <span>CONNECT</span>
         </h1>
+        <p>
+          Inspiring and Empowering the Next Gen Of tech <br /> Preneurs through
+          an advance digital platform
+        </p>
         <button>Join Waitlist</button>
       </section>
-      <section className="image"></section>
+      <section className="hero__image">
+        <Image
+          alt="theloop dashboard"
+          src={dashboard}
+          height={500}
+          width={700}
+        />
+      </section>
     </div>
   );
 };
