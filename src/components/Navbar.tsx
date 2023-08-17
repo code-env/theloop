f9a8c25968003b14b3929554e61ce5dae36bc87c
JosiasAurel <ndjosiasaurel@gmail.com>
2023-08-17 07:29:33 +0100

    separate logo && allow closing modal when clicked in empty space

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,15 +4,14 @@ import { useHeaderRoutes } from "@/constants";
 // import "@/styles/navbar.scss";
 import Image from "next/image";
 import Link from "next/link";
+import Logo from "../components/logo";
 
 const Navbar = () => {
   const routes = useHeaderRoutes();
   return (
     <header className="h-20 flex items-center sticky top-0">
       <nav className="w-full flex items-center justify-between">
-        <Link href="/" className="logo" style={{ display: "flex", justifyContent: "space-between"}}>
-          <Image alt="theloop logo" src="/logo.svg" width={60} height={50} /><h1 className="text-3xl relative bottom-2">🚀</h1>
-        </Link>
+        <Logo />
         <ul className="flex items-center gap-2 md:gap-4 xl:gap-8 text-lg">
           {routes.map((route, index) => (
             <li key={index} className="cursor-pointer">
