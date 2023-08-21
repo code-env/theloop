6210c70ddbbf24c56f0f6b7c93f3580d9ce6286a
code-env <org.codeen@gmail.com>
2023-08-21 18:17:29 +0100

    starting to work on the goals

--- src/components/ui/hoverkit.tsx
+++ src/components/ui/hoverkit.tsx
@@ -1,12 +1,25 @@
-import Header from "@/app/dashboard/components/header";
-import Sidebar from "@/app/dashboard/components/sidebar";
-import React from "react";
+import clsx from "clsx";
+import { FC, HTMLAttributes } from "react";
 
-const hoverkit = () => {
+interface hoverkitProps extends HTMLAttributes<HTMLSpanElement> {
+  label: string;
+  direction: "top" | "right" | "bottom" | "left";
+}
+
+const HoverKit: FC<hoverkitProps> = ({
+  label,
+  direction,
+  className,
+  ...others
+}) => {
   return (
-    <aside 
-    </aside>
+    <span
+      className={clsx("absolute w-fit bg-gray-300 text-gray-800", className)}
+      {...others}
+    >
+      {label}
+    </span>
   );
 };
 
-export default hoverkit;
+export default HoverKit;
