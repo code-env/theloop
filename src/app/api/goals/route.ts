d6ad93e1a8f9f5c2004765a5a166bb2a56ea42ff
code-env <org.codeen@gmail.com>
2023-08-23 12:38:36 +0100

    modified the goal schema with the edingtime to endingDate

--- src/app/api/goals/route.ts
+++ src/app/api/goals/route.ts
@@ -1,8 +1,20 @@
-import {} from "@prisma/client";
+import { Goal } from "@prisma/client";
+
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   try {
     const body = await req.json();
+
+    const { userId } = auth();
+
+    if (!userId)
+      return new NextResponse("Nigga you're unauthorized", {
+        status: 401,
+      });
+
+    const { title, description, endingDate } = body;
 
     const {} = body;
   } catch (error) {}
