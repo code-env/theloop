1d1351a0be5904c65d5f4a58f3ef6d5055deed0d
code-env <org.codeen@gmail.com>
2023-08-28 14:20:37 +0100

    added the onclick handler to remove the active on the card when clicked by the user

--- src/app/(root)/dashboard/components/goals/daytask.tsx
+++ src/app/(root)/dashboard/components/goals/daytask.tsx
@@ -6,22 +6,23 @@ import React, { FC, useState } from "react";
 interface DailyTask {
   day: string;
   date: string;
+  isActive: boolean;
+  onClick: () => void;
 }
 
-const DayTask: FC<DailyTask> = ({ day, date }) => {
-  const [isClicked, setIsClicked] = useState(false);
+const DayTask: FC<DailyTask> = ({ day, date, onClick, isActive }) => {
   return (
     <section
       className={cn(
         "flex-1 shadow-primary rounded-10 cursor-pointer p-2 bg-white h-fit",
-        isClicked && "h-48"
+        isActive && "h-48"
       )}
-      onClick={() => setIsClicked((prev) => !prev)}
+      onClick={onClick}
     >
       <h1
         className={cn(
           "flex flex-col gap-1 slowmo border-b-0",
-          isClicked && "border-b"
+          isActive && "border-b"
         )}
       >
         <span className="text-3xl font-semibold">{day}</span>
