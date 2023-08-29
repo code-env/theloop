1b69e0170814ffd8c25a01f248b3b086efd1e186
code-env <org.codeen@gmail.com>
2023-08-29 11:51:36 +0100

    added the title props

--- src/app/api/goals/task.ts
+++ src/app/api/goals/task.ts
@@ -3,30 +3,32 @@ import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
-    try {
-        const body = await req.json();
+  try {
+    const body = await req.json();
 
-        const { userId } = auth();
+    const { userId } = auth();
 
-        if (!userId) {
-            return new NextResponse("Unauthorized", {
-                status: 401
-            });
-        }
-
-        const { description, goalId } = body; 
-        await prismadb.goalTask.create({
-            data: {
-                description, 
-                goalId,
-                completed: false
-            }
-        });
-
-        return NextResponse.json({
-            msg: "Success"
-        });
-    } catch {
-        return new NextResponse("Failed to created task");
+    if (!userId) {
+      return new NextResponse("Unauthorized", {
+        status: 401,
+      });
     }
+
+    const { description, goalId, title } = body;
+    await prismadb.goalTask.create({
+      data: {
+        description,
+        goalId,
+        completed: false,
+
+        title,
+      },
+    });
+
+    return NextResponse.json({
+      msg: "Success",
+    });
+  } catch {
+    return new NextResponse("Failed to created task");
+  }
 }
