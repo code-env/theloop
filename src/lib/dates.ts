114fa739b1e421264674b11dd71f4144dfc3d468
code-env <org.codeen@gmail.com>
2023-08-30 20:12:58 +0100

    changes in the schame.prisma file

--- src/lib/dates.ts
+++ src/lib/dates.ts
@@ -1,44 +1,44 @@
-import prismadb from "./db";
-// Get today's date in the format "YYYY-MM-DD".
-const today = new Date().toISOString().split("T")[0];
+// import prismadb from "./db";
+// // Get today's date in the format "YYYY-MM-DD".
+// const today = new Date().toISOString().split("T")[0];
 
-// Calculate the next two days.
-const tomorrow = new Date();
-tomorrow.setDate(tomorrow.getDate() + 1);
-const nextDay = tomorrow.toISOString().split("T")[0];
+// // Calculate the next two days.
+// const tomorrow = new Date();
+// tomorrow.setDate(tomorrow.getDate() + 1);
+// const nextDay = tomorrow.toISOString().split("T")[0];
 
-const nextTwoDays = new Date();
-nextTwoDays.setDate(nextTwoDays.getDate() + 2);
-const dayAfterTomorrow = nextTwoDays.toISOString().split("T")[0];
+// const nextTwoDays = new Date();
+// nextTwoDays.setDate(nextTwoDays.getDate() + 2);
+// const dayAfterTomorrow = nextTwoDays.toISOString().split("T")[0];
 
-async function () {
-  const goalsForToday = await prismadb.goal.findMany({
-    where: {
-      date: today,
-    },
-  });
+// async function () {
+//   const goalsForToday = await prismadb.goal.findMany({
+//     where: {
+//       date: today,
+//     },
+//   });
 
-  const goalsForTomorrow = await prismadb.goal.findMany({
-    where: {
-      date: nextDay,
-    },
-  });
+//   const goalsForTomorrow = await prismadb.goal.findMany({
+//     where: {
+//       date: nextDay,
+//     },
+//   });
 
-  const goalsForDayAfterTomorrow = await prismadb.goal.findMany({
-    where: {
-      date: dayAfterTomorrow,
-    },
-  });
+//   const goalsForDayAfterTomorrow = await prismadb.goal.findMany({
+//     where: {
+//       date: dayAfterTomorrow,
+//     },
+//   });
 
-  console.log("Goals for Today:", goalsForToday);
-  console.log("Goals for Tomorrow:", goalsForTomorrow);
-  console.log("Goals for Day After Tomorrow:", goalsForDayAfterTomorrow);
-}
+//   console.log("Goals for Today:", goalsForToday);
+//   console.log("Goals for Tomorrow:", goalsForTomorrow);
+//   console.log("Goals for Day After Tomorrow:", goalsForDayAfterTomorrow);
+// }
 
-getGoalsForSpecificDays()
-  .catch((error) => {
-    console.error("Error:", error);
-  })
-  .finally(async () => {
-    await prismadb.$disconnect();
-  });
+// getGoalsForSpecificDays()
+//   .catch((error) => {
+//     console.error("Error:", error);
+//   })
+//   .finally(async () => {
+//     await prismadb.$disconnect();
+//   });
