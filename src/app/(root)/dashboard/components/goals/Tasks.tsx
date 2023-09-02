b22f115834df1cb83336abb08e1afd0edc88ec06
code-env <org.codeen@gmail.com>
2023-09-02 08:59:28 +0100

    fixed the deloyment error

--- src/app/(root)/dashboard/components/goals/Tasks.tsx
+++ src/app/(root)/dashboard/components/goals/Tasks.tsx
@@ -1,47 +1,47 @@
-"use client";
+// "use client";
 
-// import { getUserGoals } from "@/lib/user.actions/getUserGoals";
-import axios from "axios";
-import React, { FC, useEffect, useState } from "react";
-import ThreeDayTaskSection from "./threeDayTaskSection";
-import { userGoals } from "@/lib/types";
+// // import { getUserGoals } from "@/lib/user.actions/getUserGoals";
+// import axios from "axios";
+// import React, { FC, useEffect, useState } from "react";
+// import ThreeDayTaskSection from "./threeDayTaskSection";
+// import { userGoals } from "@/lib/types";
 
-interface TasksProps {
-  userId: string;
-}
+// interface TasksProps {
+//   userId: string;
+// }
 
-const Tasks: FC<TasksProps> = ({ userId }) => {
-  const [goals, setGoals] = useState<userGoals>([]);
-  const [isClient, setIsClient] = useState(false);
+// const Tasks: FC<TasksProps> = ({ userId }) => {
+//   const [goals, setGoals] = useState<userGoals>([]);
+//   const [isClient, setIsClient] = useState(false);
 
-  // console.log(userId);
+//   // console.log(userId);
 
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
+//   useEffect(() => {
+//     setIsClient(true);
+//   }, []);
 
-  useEffect(() => {
-    const getThreeDaysGoal = async () => {
-      try {
-        const { data } = await axios.get(`/api/goals/${userId}`);
+//   useEffect(() => {
+//     const getThreeDaysGoal = async () => {
+//       try {
+//         const { data } = await axios.get(`/api/goals/${userId}`);
 
-        setGoals(data);
+//         setGoals(data);
 
-        console.log("newGoals");
-      } catch (error: any) {
-        console.log(error.message);
-      }
-    };
+//         console.log("newGoals");
+//       } catch (error: any) {
+//         console.log(error.message);
+//       }
+//     };
 
-    getThreeDaysGoal();
-  }, []);
+//     getThreeDaysGoal();
+//   }, []);
 
-  return (
-    <section className="mt-6">
-      <h1 className="header text-xl font-semibold ">Tasks</h1>
-      <ThreeDayTaskSection goals={goals} />
-    </section>
-  );
-};
+//   return (
+//     <section className="mt-6">
+//       <h1 className="header text-xl font-semibold ">Tasks</h1>
+//       <ThreeDayTaskSection goals={goals} />
+//     </section>
+//   );
+// };
 
-export default Tasks;
+// export default Tasks;
